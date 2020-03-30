d91d4506b0cf2e656fb9512a9ce71c9c3d4ef3c1
Ryan Parrish <rino3717@gmail.com>
2020-03-30 20:25:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,18 @@
 class Dog 
+  
+  def  initialize(name)
+    @name = name 
+  end
+  
   def initialize(breed)
     @breed =  breed
   end
+  
+  def breed=(breed)
+    @breed =  breed 
+  end
+  
+  def breed 
+    @breed 
 end
+
